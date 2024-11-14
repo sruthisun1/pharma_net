2b87a9d74d641503ffd5f4b5c57e94ab1826a537
sruthisun1 <sruthis4@illinois.edu>
2024-11-14 16:29:20 -0600

    Initial Commit

---
+++ pharma_net/pharma_netApp.swift
@@ -0,0 +1,20 @@
+//
+//  pharma_netApp.swift
+//  pharma_net
+//
+//  Created by Sruthi Sundar on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct pharma_netApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
